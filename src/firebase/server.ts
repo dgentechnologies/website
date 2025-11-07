7d5526fdba90c700f2e12bcd358dd2b6bfc88e18
DGEN TECHNOLOGIES <dgentechnologiespltd@gmail.com>
2025-11-07 13:17:51 +0000

    Try fixing this error: `Console Error: Error: Failed to parse private ke

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -4,25 +4,30 @@ import { getFirestore, Firestore } from 'firebase-admin/firestore';
 import 'dotenv/config'
 
 let app: App;
+let firestore: Firestore;
 
 if (!getApps().length) {
-  if(process.env.FIREBASE_PRIVATE_KEY) {
+  const hasCredentials = process.env.FIREBASE_PROJECT_ID && process.env.FIREBASE_CLIENT_EMAIL && process.env.FIREBASE_PRIVATE_KEY;
+
+  if (hasCredentials) {
     const serviceAccount = {
       projectId: process.env.FIREBASE_PROJECT_ID!,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-    }
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
+    };
     app = initializeApp({
       credential: cert(serviceAccount)
     });
   } else {
     // This will use Application Default Credentials
+    // Useful for deployments to Google Cloud services
+    console.log("Initializing Firebase Admin with Application Default Credentials");
     app = initializeApp();
   }
 } else {
   app = getApp();
 }
 
-const firestore: Firestore = getFirestore(app);
+firestore = getFirestore(app);
 
 export { app, firestore };
