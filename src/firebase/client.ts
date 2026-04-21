a07d55c9a3a7e9db1df8fcb1367c4c8024e4e3b6
Tirthankar Dasgupta <tirthankardasgupta913913@gmail.com>
2026-04-21 21:41:45 +0530

    fix3

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -7,7 +7,11 @@ import firebaseConfig from './config';
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const firestore = getFirestore(app);
+const databaseId =
+	process.env.NEXT_PUBLIC_FIREBASE_DATABASE_ID ||
+	process.env.NEXT_PUBLIC_FIREBASE_WEBSITE_DATABASE_ID ||
+	'(default)';
+const firestore = getFirestore(app, databaseId);
 const auth = getAuth(app);
 
 export { app, firestore, auth };
